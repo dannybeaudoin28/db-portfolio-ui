653ccc62411f8240782e3756869c60f395c2808f
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2023-11-20 19:52:15 -0500

    initial commit

---
+++ src/components/home-component/home.component.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    const test = 1;
+    
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>asdfasdfasdf</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/navigation-component/navigation.component.jsx
@@ -0,0 +1,12 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+export default Navigation;
