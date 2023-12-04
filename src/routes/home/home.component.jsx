51fe0157df2224ae62823e12a74483a84af78e45
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2023-12-04 09:02:11 -0500

    add content to home page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,14 +1,22 @@
-const Home = () => {
-    const test = 1;
-    
+import './home.styles.scss';
+
+const Home = () => {    
     return (
         <div>
-            <h1>I'm Danny</h1>
-            <p>
-                More information coming soon!..
-                <br/><br/>
-                Contact me at <a href="mailto: dannybeaudoin@dannybeaudoin613.com">dannybeaudoin@dannybeaudoin613.com</a>
+            <h1 className='about-content'>
+                With a deep understanding of back-end development best practices, I am an innovative developer 
+                experienced in Java and Spring-Boot. I deliver high-quality products that meet client's needs.
+            </h1>
+            <h2></h2>
+            <p className='about-content'></p>
+            <h2>My Background:</h2>
+            <p className='about-content'>
+                Growing up in a small town there wasn't a whole lot to do.. This led me to discover a passion for a brand new hobby... computers
             </p>
+            <h2>My Education:</h2>
+            <p className='about-content'>Graduated class of 2023 of the Computer Programming & Analysis program at St. Lawrence College</p>
+            <h2>Contact Me:</h2>
+            <p className="about-content">Contact me at <a href="mailto: dannybeaudoin@dannybeaudoin613.com">dannybeaudoin@dannybeaudoin613.com</a></p>
         </div>
     );
 };
